3d79f5209b69159808cbda6694b8d97d757fbc52
Pepe Becker <mail@pepebecker.com>
2018-03-07 04:56:32 +0100

    Fix showKeyboard bug

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const state = {
   started: false,
   pause: false,
   lastTime: 0,
-  show_keyboard: true,
+  showKeyboard: true,
   dom: {
     setup: document.querySelector('.setup'),
     start: document.querySelector('.start_btn'),
