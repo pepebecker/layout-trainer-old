ed1e1c3c316b08b77e138b2c9702ce0875db8252
Pepe Becker <mail@pepebecker.com>
2018-04-29 01:48:47 +0100

    Remove English (U.K.)

--- src/languages/index.js
+++ src/languages/index.js
@@ -2,10 +2,8 @@
 
 module.exports = {
   en_us: require('./en-us'),
-  en_gb: require('./en-gb'),
   de_de: require('./de-de'),
   es_es: require('./es-es'),
-  // jp:    require('./jp'   ),
   ko:    require('./ko'   ),
   ru:    require('./ru'   ),
   zh_tw: require('./zh-tw')
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,9 +5,7 @@ const en_us = require('./en-us')
 module.exports = {
   en_us,
   de_de: Object.assign({}, en_us, require('./de-de')),
-  en_gb: Object.assign({}, en_us, require('./en-gb')),
   es_es: Object.assign({}, en_us, require('./es-es')),
-  jp:    Object.assign({}, en_us, require('./jp'   )),
   ko:    Object.assign({}, en_us, require('./ko'   )),
   ru:    Object.assign({}, en_us, require('./ru'   )),
   zh_tw: Object.assign({}, en_us, require('./zh-tw'))
